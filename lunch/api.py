e56af7d68cff7a964dabe644934e461df31816ae
Jan Dobes <git@owny.cz>
2019-07-30 19:50:35 +0200

    support getting menus

--- lunch/api.py
+++ lunch/api.py
@@ -1,8 +1,10 @@
+from datetime import date
 import logging
 
 import flask
+from peewee import JOIN
 
-from .model import Restaurant
+from .model import Restaurant, RestaurantMenu
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -26,4 +28,26 @@ def get_restaurants():
     for restaurant in Restaurant.select():
         response["restaurants"].append({"label": restaurant.label, "name": restaurant.name,
                                         "url": restaurant.url})
+    return response
+
+
+def get_menus(day=None, restaurants=None):
+    if day:
+        try:
+            requested_date = date.fromisoformat(day)
+        except ValueError:
+            return "Invalid day format: %s" % day, 400
+    else:
+        requested_date = date.today()
+
+    query = Restaurant.select(Restaurant.label, RestaurantMenu.menu).join(RestaurantMenu, JOIN.LEFT_OUTER) \
+        .where((RestaurantMenu.day == requested_date) | (RestaurantMenu.day.is_null()))
+    if restaurants:
+        requested_restaurants = restaurants.split(",")
+        query = query.where(Restaurant.label << requested_restaurants)
+
+    response = {"day": requested_date, "menus": []}
+    for menu in query.dicts():
+        response["menus"].append({"restaurant": menu["label"], "menu": menu["menu"]})
+
     return response
