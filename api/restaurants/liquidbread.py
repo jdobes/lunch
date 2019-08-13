940e2dfde675939452ddf0f78e4261037d902163
Jan Dobes <git@owny.cz>
2019-08-13 14:14:15 +0200

    add liquid bread

--- api/restaurants/liquidbread.py
+++ api/restaurants/liquidbread.py
@@ -1,7 +1,45 @@
 # -*- coding: utf-8 -*-
+from datetime import date, timedelta
+import logging
+
+from .utils import fetch_html
 
 NAME = "Liquid Bread"
 URL = "http://www.liquidbread.cz/liquidbread/liquidbread/mainmenu/daily-menu/"
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 def parse_menu():
-    return None
+    today = date.today()
+    html = fetch_html(URL)
+    result = {}
+    if html:
+        start_day = None
+        for p in html.find_all("p", {"align": "center"}):
+            if p.text.startswith("Polední menu"):
+                date_interval = p.text.split()[2]
+                end_date = date_interval.split("-")[1]
+                end_date_parts = end_date.split(".")
+                end_date = date(int(end_date_parts[2]),
+                                day=int(end_date_parts[0]),
+                                month=int(end_date_parts[1]))
+                # FIXME: doesn't work for shorter weeks
+                start_day = end_date - timedelta(days=4)
+        
+        if start_day:
+            current_date = None
+            for span in html.find_all("span", {"style": "font-size: small;"}):
+                if span.text.startswith("PO") or span.text.startswith("ÚT") or span.text.startswith("ST") or \
+                    span.text.startswith("ČT") or span.text.startswith("PÁ"):
+                    if current_date is None:
+                        current_date = start_day
+                    else:
+                        current_date += timedelta(days=1)
+                    if today <= current_date:
+                        result[current_date] = [span.text]
+                elif current_date and current_date in result:
+                    result[current_date].append(span.text)
+    return result
+
