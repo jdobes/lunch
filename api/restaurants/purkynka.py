febf9ec0721448a7d555140e0a6dc23adc67f29a
Jan Dobes <git@owny.cz>
2019-08-13 12:03:55 +0200

    add purkynka

--- api/restaurants/purkynka.py
+++ api/restaurants/purkynka.py
@@ -1,7 +1,42 @@
 # -*- coding: utf-8 -*-
+from datetime import date, timedelta
+import logging
+
+from .utils import fetch_html
 
 NAME = "Na Purkyňce"
 URL = "http://www.napurkynce.cz/denni-menu/"
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 def parse_menu():
-    return None
+    today = date.today()
+    html = fetch_html(URL)
+    result = {}
+    if html:
+        start_day = None
+        for p in html.find_all("p", {"align": "center"}):
+            if p.text.startswith("Polední menu"):
+                date_interval = p.text.split()[2]
+                end_date = date_interval.split("-")[1]
+                end_date_parts = end_date.split(".")
+                end_date = date(int(end_date_parts[2]),
+                                day=int(end_date_parts[0]),
+                                month=int(end_date_parts[1]))
+                start_day = end_date - timedelta(days=4)
+        
+        if start_day:
+            current_date = None
+            for span in html.find_all("span", {"style": "font-size: small;"}):
+                if span.find("strong"):
+                    if current_date is None:
+                        current_date = start_day
+                    else:
+                        current_date += timedelta(days=1)
+                    if today <= current_date:
+                        result[current_date] = [span.text]
+                elif current_date and current_date in result:
+                    result[current_date].append(span.text)
+    return result
