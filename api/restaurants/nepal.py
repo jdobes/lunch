62540c895922d0fdbe8567dfa193771ff5974d8d
Jan Dobes <git@owny.cz>
2021-12-02 10:25:44 +0100

    fix nepal

--- api/restaurants/nepal.py
+++ api/restaurants/nepal.py
@@ -18,24 +18,27 @@ def parse_menu():
     result = {}
     if html:
         current_date = None
-        table = html.find("table")
-        if table:
-            for td in table.find_all("td"):
+        for tr in html.find_all("tr"):
+            for td in tr.find_all("td"):
                 date_span = td.find("span")
                 if date_span:
-                    if "PONDĚLÍ" in date_span.text.upper() or \
-                        "ÚTERÝ" in date_span.text.upper() or \
-                            "STŘEDA" in date_span.text.upper() or \
-                                "ČTVRTEK" in date_span.text.upper() or \
-                                    "PÁTEK" in date_span.text.upper():
-                                    if current_date is None:
-                                        current_date = last_monday
-                                    else:
-                                        current_date += timedelta(days=1)
-                    result[current_date] = []
+                    if "MONDAY" in date_span.text.upper():
+                        current_date = last_monday
+                    elif "TUESDAY" in date_span.text.upper():
+                        current_date = last_monday + timedelta(days=1)
+                    elif "WEDNESDAY" in date_span.text.upper():
+                        current_date = last_monday + timedelta(days=2)
+                    elif "THURSDAY" in date_span.text.upper():
+                        current_date = last_monday + timedelta(days=3)
+                    elif "FRIDAY" in date_span.text.upper():
+                        current_date = last_monday + timedelta(days=4)
+                    else:
+                        current_date = None
+                    if current_date:
+                        result[current_date] = []
                 elif current_date:
                     if td.text.endswith("Kč"):
                         result[current_date][-1] += " %s" % td.text
-                    elif not td.text.strip().isspace() and td.text.strip() not in result[current_date]:
+                    elif not td.text.strip().isspace() and td.text.strip() and td.text.strip() not in result[current_date]:
                         result[current_date].append(td.text.strip())
     return result
