eb132026a3dce7ab0ea8fab8aa5e540068cf5154
Jan Dobes <git@owny.cz>
2023-03-06 13:38:00 +0100

    remove zomato

--- api/restaurants/portoriko.py
+++ api/restaurants/portoriko.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-from .utils import fetch_zomato, parse_zomato
-
 NAME = "Portoriko"
-URL = "https://www.zomato.com/cs/brno/portoriko-kr%C3%A1lovo-pole-brno-sever/denn%C3%AD-menu"
-RESTAURANT_ID = "16506862"
+URL = ""
 
 
 def parse_menu():
-    zomato_json = fetch_zomato(RESTAURANT_ID)
-    return parse_zomato(zomato_json)
-
+    return {}
--- api/restaurants/utils.py
+++ api/restaurants/utils.py
@@ -8,10 +8,6 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-ZOMATO_API_KEY = os.getenv("ZOMATO_API_KEY", "")
-ZOMATO_API_URL = "https://developers.zomato.com/api/v2.1/dailymenu?res_id="
-ZOMATO_API_HEADERS = {"Accept": "application/json", "user_key": ZOMATO_API_KEY}
-
 MENICKA_URL = "https://www.menicka.cz/api/iframe/?id=%s"
 
 
@@ -30,38 +26,8 @@ def fetch_html(url, encoding="utf-8"):
     return None
 
 
-def fetch_zomato(restaurant_id):
-    if ZOMATO_API_KEY:
-        for _ in range(3):
-            try:
-                response = requests.get("%s%s" % (ZOMATO_API_URL, restaurant_id), headers=ZOMATO_API_HEADERS)
-                if response.status_code == 200:
-                    return response.json()
-            except requests.exceptions.RequestException:
-                logger.warning("Error fetching url.")
-    else:
-        logger.error("Zomato API key is not specified.")
-    logger.error("URL not fetched.")
-    return None
-
-
 def fetch_menicka(restaurant_id):
     return fetch_html(MENICKA_URL % restaurant_id, encoding="windows-1250")
-
-
-def parse_zomato(zomato_json):
-    result = {}
-    if zomato_json:
-        daily_menus = zomato_json["daily_menus"]
-        for daily_menu in daily_menus:
-            daily_menu = daily_menu["daily_menu"]
-            current_date = datetime.strptime(daily_menu["start_date"].split()[0], "%Y-%m-%d").date()
-            result[current_date] = []
-            for dish in daily_menu["dishes"]:
-                dish = dish["dish"]
-                result[current_date].append("%s %s" % (dish["name"], dish["price"]))
-
-    return result
 
 
 def parse_menicka(html):
