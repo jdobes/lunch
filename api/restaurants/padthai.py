bd129153a2dc8d91043ac847bfa5dc04488e073b
Jan Dobes <git@owny.cz>
2023-04-24 11:07:30 +0200

    fix portoriko and padthai

--- api/restaurants/padthai.py
+++ api/restaurants/padthai.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from .utils import fetch_menicka, parse_menicka
+
 NAME = "Pad Thai"
-URL = ""
+URL = "https://www.menicka.cz/8483-padthai.html"
+RESTAURANT_ID = "8483"
 
 
 def parse_menu():
-    return {}
+    menicka_html = fetch_menicka(RESTAURANT_ID)
+    return parse_menicka(menicka_html)
--- api/restaurants/portoriko.py
+++ api/restaurants/portoriko.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from .utils import fetch_menicka, parse_menicka
+
 NAME = "Portoriko"
-URL = ""
+URL = "https://www.menicka.cz/8467-portoriko.html"
+RESTAURANT_ID = "8467"
 
 
 def parse_menu():
-    return {}
+    menicka_html = fetch_menicka(RESTAURANT_ID)
+    return parse_menicka(menicka_html)
