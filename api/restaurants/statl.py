df065d445d293ca4a7e9eb6070794a9323969da4
Tibor Dudlák <tibor.dudlak@gmail.com>
2024-11-22 15:48:33 +0100

    Add restaurants from city centre
    Add few restaurants from city centre and to enabled list
    
    Signed-off-by: Tibor Dudlák <tibor.dudlak@gmail.com>

---
+++ api/restaurants/statl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from .utils import fetch_menicka, parse_menicka
+
+NAME = "Statl"
+URL = "https://www.menicka.cz/4946-statl.html"
+RESTAURANT_ID = "4946"
+GPS = (49.1955397, 16.6048724)
+
+
+def parse_menu():
+    menicka_html = fetch_menicka(RESTAURANT_ID)
+    return parse_menicka(menicka_html)
