6bc8360d4cb6e66ceba103bca75a044da9f00191
Jan Dobes <git@owny.cz>
2025-06-16 00:18:32 +0200

    fix: nepal has new page

--- api/restaurants/nepal.py
+++ api/restaurants/nepal.py
@@ -5,7 +5,7 @@ import logging
 from .utils import fetch_html
 
 NAME = "TikTok Nepal"
-URL = "https://nepalbrno.cz/weekly-menu/"
+URL = "https://nepalbrno.cz/poledni.php"
 GPS = (49.22448917714069, 16.59026497848504)
 
 logger = logging.getLogger(__name__)
@@ -19,22 +19,25 @@ def parse_menu():
     result = {}
     if html:
         current_date = None
-        for tr in html.find_all("tr"):
-            for td in tr.find_all("td"):
-                txt = td.text.strip()
-                if "MONDAY" in txt.upper():
-                    current_date = last_monday
-                elif "TUESDAY" in txt.upper():
-                    current_date = last_monday + timedelta(days=1)
-                elif "WEDNESDAY" in txt.upper():
-                    current_date = last_monday + timedelta(days=2)
-                elif "THURSDAY" in txt.upper():
-                    current_date = last_monday + timedelta(days=3)
-                elif "FRIDAY" in txt.upper():
-                    current_date = last_monday + timedelta(days=4)
-                elif current_date:
-                    if txt.endswith("Kč"):
-                        result[current_date][-1] += " %s" % txt  # should always follow a meal
-                    elif txt:  # filter out empty strings
-                        result.setdefault(current_date, []).append(txt)
+        for day_section in html.find_all("div", {"class": "day-section"}):
+            day_title = day_section.find("h2", {"class": "day-title"})
+            if not day_title:
+                continue
+            day_title = day_title.text.strip().upper()
+            if "MONDAY" in day_title:
+                current_date = last_monday
+            elif "TUESDAY" in day_title:
+                current_date = last_monday + timedelta(days=1)
+            elif "WEDNESDAY" in day_title:
+                current_date = last_monday + timedelta(days=2)
+            elif "THURSDAY" in day_title:
+                current_date = last_monday + timedelta(days=3)
+            elif "FRIDAY" in day_title:
+                current_date = last_monday + timedelta(days=4)
+            else:
+                continue
+            for menu_item in day_section.find_all("div", {"class": "menu-item"}):
+                subtags = [subtag.text.strip() for subtag in menu_item]
+                result.setdefault(current_date, []).append(" ".join(subtags))
+
     return result
