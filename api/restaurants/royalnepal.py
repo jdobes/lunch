0c426de3181021ea749f98519ac625a3471a35c1
Juraj Marcin <juraj@jurajmarcin.com>
2026-01-16 12:17:59 +0100

    fix: parse Royal Nepal menu from home page fragment
    They removed the weekly menu page, menu on the home page is dynamically
    inserted from this route.
    
    Signed-off-by: Juraj Marcin <juraj@jurajmarcin.com>

--- api/restaurants/royalnepal.py
+++ api/restaurants/royalnepal.py
@@ -2,20 +2,14 @@
 from datetime import date, timedelta
 import logging
 
-from unidecode import unidecode
-
 from .utils import fetch_html
 
 NAME = "Royal Nepal"
-URL = "https://www.royalnepal.cz/kralovo-pole/weekly-menu"
+URL = "https://www.royalnepal.cz/MC/HANDLERS/getDocument.php?name=WEEKLY&location=kralovopole&language=cz"
 GPS = (49.22052154321084, 16.58709101916278)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-def format_line(row):
-    return " ".join([col.text.strip().replace("\n", "") for col in row.find_all("div", recursive=False)])
 
 
 def parse_menu():
@@ -24,19 +18,15 @@ def parse_menu():
     html = fetch_html(URL)
     result = {}
     if html:
-        current_date = None
-        for row in html.find_all("div", {"class": "row"}):
-            txt = format_line(row)
-            if "pondeli" in unidecode(txt.lower()):
-                current_date = last_monday
-            elif "utery" in unidecode(txt.lower()):
-                current_date = last_monday + timedelta(days=1)
-            elif "streda" in unidecode(txt.lower()):
-                current_date = last_monday + timedelta(days=2)
-            elif "ctvrtek" in unidecode(txt.lower()):
-                current_date = last_monday + timedelta(days=3)
-            elif "patek" in unidecode(txt.lower()):
-                current_date = last_monday + timedelta(days=4)
-            elif current_date and txt:
-                result.setdefault(current_date, []).append(txt)
+        for weekday, day_section in enumerate(html.find_all("div", {"class": "weekly-template-container12"})):
+            current_date = last_monday + timedelta(weekday)
+            menu = []
+            soup = day_section.find("div", {"mc-template": "soup"})
+            if soup:
+                menu.append(soup.get_text(" ", True))
+            for main in day_section.find_all("div", {"mc-template": "main"}):
+                menu.append(main.get_text(" ", True))
+            for weekly_main in day_section.find_all("div", {"mc-template": "weekly"}):
+                menu.append(weekly_main.get_text(" ", True))
+            result[current_date] = menu
     return result
